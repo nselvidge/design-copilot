323dceb5dc60367eff1f8ca772976d367bf9ab37
Aayush Mathur <aayushmathur7@gmail.com>
2023-07-15 14:47:23 -0700

    Merge

--- server/app.py
+++ server/app.py
@@ -15,10 +15,12 @@ def generate_schema():
 
     try:
         model_files = schema_generator.list_files_in_models_folder(repo_url, branch_name)
-        schema_generator.generate_json_from_models(repo_url, model_files)
+        print("REEEEEE:", model_files)
+        schema_generator.generate_json_from_models(repo_url, branch_name, model_files)
     except ValueError as e:
         return jsonify(error=str(e)), 400
     return jsonify(message="JSON file was saved as output.json"), 200
+
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
--- server/schema_generator.py
+++ server/schema_generator.py
@@ -81,6 +81,7 @@ def compute_prefix_and_zip_url(repo_url, main_branch="master"):
 def list_files_in_models_folder(repo_url, branch_name):
     zip_file, folder_prefix = zipfile_from_github(repo_url, branch_name)
 
+    print("it works.")
     model_files = []
 
     for file in zip_file.namelist():
@@ -89,6 +90,7 @@ def list_files_in_models_folder(repo_url, branch_name):
                 and not file.endswith('/')
                 and not os.path.basename(file).startswith('.')
                 and file.count('/') == 3):  # Do not read subdirectories
+            print(file)
             model_files.append(file)
 
     return model_files
@@ -148,17 +150,15 @@ def generate_json_from_models(repo_url, branch_name, model_files):
         openai_api_key=OPENAI_API_KEY, model_name="gpt-3.5-turbo-16k-0613", temperature=0.2
     )
     for file in model_files:
-        print(file)
         file_content = get_file_content(repo_url, branch_name, file)
         # Here we use the function read_prompt_from_file to get the prompt template
-        prompt_template = read_prompt_from_file("../prompts/json_generator.txt")
+        prompt_template = read_prompt_from_file("server/prompts/json_generator.txt")
         prompt = PromptTemplate(
             template=prompt_template,
             input_variables=["file_content", "example_schema"]
         )
         llm_chain = LLMChain(prompt=prompt, llm=llm)
         output = llm_chain.run({'file_content': file_content, 'example_schema': example_schema})
-
         # Convert the output from string format to JSON object
         json_output = json.loads(output)
 
@@ -168,7 +168,7 @@ def generate_json_from_models(repo_url, branch_name, model_files):
         json_model_dict[class_name] = json_output[class_name]
 
     # Save json_model_dict to a JSON file
-    with open('../output.json', 'w') as json_file:
+    with open('output.json', 'w') as json_file:
         json.dump(json_model_dict, json_file, indent=4)
 
 
