7ad9c66e138e175eb2f366e00fa2f444c0deb481
Aayush Mathur <aayushmathur7@gmail.com>
2023-07-15 13:39:35 -0700

    Minor fix

--- schema_generator.py
+++ schema_generator.py
@@ -1,4 +1,3 @@
-import base64
 import os
 import zipfile
 from io import BytesIO
@@ -7,11 +6,11 @@ from urllib.request import urlopen
 from langchain import PromptTemplate
 from langchain.chains import LLMChain
 from langchain.chat_models import ChatOpenAI
-
+import base64
 from dotenv import load_dotenv
 from ghapi.all import GhApi
 
-load_dotenv(".env")
+load_dotenv('.env')
 GH_TOKEN = os.getenv("GH_TOKEN", "")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "")
 
@@ -62,31 +61,11 @@ def list_files_in_models_folder(repo_url):
 
     for file in zip_file.namelist():
         # Check if the file is in the "models" folder
-        if (
-            file.startswith(f"{folder_prefix}/app/models")
-            and not file.endswith("/")
-            and not os.path.basename(file).startswith(".")
-        ):
+        if file.startswith(f"{folder_prefix}/app/models") and not file.endswith('/') and not os.path.basename(
+                file).startswith('.'):
             model_files.append(file)
 
     return model_files
-
-
-def generate_json_from_models(repo_url, model_files):
-    llm = ChatOpenAI(
-        openai_api_key=OPENAI_API_KEY, model_name="gpt-3.5-turbo-16k-0613", temperature=0.2
-    )
-    for file in model_files:
-        file_content = get_file_content(repo_url, file)
-        prompt = PromptTemplate(
-            template="""
-            Convert below model into a JSON format that give the entire context of the models. Do not include any text before or after the JSON.
-            Here is the model: {file_content}
-            """, 
-            input_variables=file_content
-        )
-        llm_chain = LLMChain(prompt=prompt, llm=llm)
-        print(llm_chain.run(file_content))
 
 
 def get_file_content(repo_url, file_path):
@@ -112,9 +91,7 @@ def get_file_content(repo_url, file_path):
     if commits:
         latest_commit_sha = commits[0].sha
         try:
-            file_content = api.repos.get_content(
-                username, repo, path=relative_file_path, ref=latest_commit_sha
-            )
+            file_content = api.repos.get_content(username, repo, path=relative_file_path, ref=latest_commit_sha)
             decoded_content = base64.b64decode(file_content.content).decode("utf-8")
             num_lines = len(decoded_content.splitlines())
 
@@ -133,9 +110,26 @@ def get_file_content(repo_url, file_path):
     return None
 
 
+def generate_json_from_models(repo_url, model_files):
+    json_model_array = []
+    llm = ChatOpenAI(
+        openai_api_key=OPENAI_API_KEY, model_name="gpt-3.5-turbo-16k-0613", temperature=0.2
+    )
+    for file in model_files:
+        file_content = get_file_content(repo_url, file)
+        prompt = PromptTemplate(
+            template="""
+            Convert below model into a JSON format that give the entire context of the models. Do not include any text before or after the JSON.
+            Here is the model: {file_content}
+            """, 
+            input_variables=file_content
+        )
+        llm_chain = LLMChain(prompt=prompt, llm=llm)
+        output = llm_chain.run(file_content)
+        json_model_array.append(output)
+        print(json_model_array)
+
 if __name__ == "__main__":
     repo_url = "https://github.com/gothinkster/rails-realworld-example-app"
     model_files = list_files_in_models_folder(repo_url)
     generate_json_from_models(repo_url, model_files)
-
-    # print(file_content)
